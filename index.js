346527c95ec5679ac51c896d4c50fac8b6b27c0d
Chandru AIML-A <231501031@rajalakshmi.edu.in>
2025-01-24 20:59:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function gmail(){
+    window.location.href = "https://mail.google.com/mail/u/1/#inbox";
+}
+function search(){
+    window.location.href = "https://labs.google.com/search?source=hp&authuser=0";
+}
